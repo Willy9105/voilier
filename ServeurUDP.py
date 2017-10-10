de5a3cf55c8ae6f2deff7b2c92fd83493a213f6c
Willy9105 <larreura@lacroixrouge-brest.fr>
2017-10-10 11:52:09 +0200

    Ajout de l'envoi de la trame de réponse vers le client

--- ServeurUDP.py
+++ ServeurUDP.py
@@ -2,14 +2,29 @@
 # -*- coding: utf-8 -*-
 
 import socket
-IPserv="192.168.0.232"
-Port=5005
+ip="127.0.0.1"
+port=5005
+idTrame=35
+tailleTrame=5
+vVent=42
+dVent=57
+oGite=77
+
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind((IPserv,Port))
+sock.bind((ip,port))
+
+
 
 while True:
 	data,addr=sock.recvfrom(1024)
+	trame_reponse=bytearray([idTrame,tailleTrame,vVent,dVent,oGite])
+	sock.sendto(trame_reponse,(addr[0],addr[1]))
+	
+	print 'ID de trame :', ord(data[0])
+	print 'Taille de la trame :', ord(data[1])
+	print 'Orientation safran :', ord(data[2]),'°'
+	print 'Orientation de la GV :', ord(data[3]),'°'
 
-	print data
+
 
